8a985e24f4330e18d90b3f9086ce6ad0dd1a45a2
佟亚鹏 <yapeng.tong@baidao.comc>
2017-03-12 22:06:34 +0800

    remove

--- fastdex-build/colze/src/main/java/com/example/colze/bean/BaseBean.java
+++
@@ -1,6 +0,0 @@
-package com.example.colze.bean;
-
-public class BaseBean {
-	public int status;
-	public String msg;
-}
--- fastdex-build/colze/src/main/java/com/example/colze/utils/pay/PayListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.colze.utils.pay;
-
-public interface PayListener {
-	public void onPaySuccess();
-
-	public void onPaying();
-
-	public void onPayErr();
-}
